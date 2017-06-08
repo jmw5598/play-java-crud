c2ff94a8b4a07b596778abaf85a97c46f0c92f94
Jason White <jmw5598@gmail.com>
2017-06-08 16:59:07 -0400

    handled bad resposnes

--- contacts/app/controllers/ContactController.java
+++ contacts/app/controllers/ContactController.java
@@ -6,6 +6,7 @@ import play.mvc.*;
 import play.libs.Json;
 import service.ContactService;
 import model.Contact;
+import util.ResponseBuilder;
 /**
  * This controller contains an action to handle HTTP requests
  * to the application's home page.
@@ -26,22 +27,43 @@ public class ContactController extends Controller {
     public Result createContact() {
       JsonNode json = request().body().asJson();
       Contact contact = contactService.add(Json.fromJson(json, Contact.class));
-      return ok(Json.toJson(contact));
+      if(contact == null) {
+        return badRequest(
+          ResponseBuilder.build("Error creating new contact", false)
+        );
+      }
+      return ok(ResponseBuilder.build(contact, true));
     }
 
     public Result getContactById(Integer id) {
-      return ok(Json.toJson(contactService.findById(id)));
+      Contact contact = contactService.findById(id);
+      if(contact == null) {
+        return notFound(
+          ResponseBuilder.build("Unable to find contact with id : " + id, false)
+        );
+      }
+      return ok(ResponseBuilder.build(contact, true));
     }
 
     public Result updateContact() {
       JsonNode json = request().body().asJson();
       Contact contact = contactService.update(Json.fromJson(json, Contact.class));
-      return ok(Json.toJson(contact));
+      if(contact == null) {
+        return badRequest(
+          ResponseBuilder.build("Error while updating contact", false)
+        );
+      }
+      return ok(ResponseBuilder.build(contact, true));
     }
 
     public Result deleteContact(Integer id) {
       Contact contact = contactService.remove(id);
-      return ok(Json.toJson(contact));
+      if(contact == null) {
+        return badRequest(
+          ResponseBuilder.build("Error while deleting contact with id: " + id, false)
+        );
+      }
+      return ok(ResponseBuilder.build(contact, true));
     }
 
 }
